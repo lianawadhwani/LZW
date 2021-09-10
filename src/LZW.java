1b86c3a7dafa52a4fc09edde7734b1b77823f186
lianawadhwani <89544220+lianawadhwani@users.noreply.github.com>
2021-09-10 12:06:09 -0700

    add comments

--- src/LZW.java
+++ src/LZW.java
@@ -10,33 +10,33 @@ import java.io.IOException;
 		private String word; 
 		private int size=255; 
 		private HashMap <String,Integer> dictionary = new HashMap <String,Integer> (); 
-		private ArrayList <String> tracker = new ArrayList <String> (); 
-		private ArrayList <Integer> nums = new ArrayList <Integer> (); 
+		private ArrayList <String> tracker = new ArrayList <String> (); // this keeps track of the output 
+		private ArrayList <Integer> nums = new ArrayList <Integer> (); // to keep track of the ASCII code 
 		
 
 		public LZW () throws IOException{
-			word=""; 
+			word=""; // intalizes word
 			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("/Users/lianawadhwani/Desktop/test.txt"))); // this line reads the file
-			String line = reader.readLine();
-			while (line != null) {
+			String line = reader.readLine(); 
+			while (line != null) { // reads the file 
 				word+=line; 
 				line= reader.readLine();
 			}
-			for (int i=0; i<256; i++) {
+			for (int i=0; i<256; i++) { // creates the dictionary and puts the values in it 
 				dictionary.put(""+(char)i,i); // this line takes a number converts in to a char and then corresponds that to the number
 				
 			}
 		}
 
 		public void algorithim () {
-			String current=word.substring(0,1); 
-			String next=word.substring(1,2); 
-			while (word.length()>0) {
-				if (dictionary.containsKey(""+current+next)==false && word.length()>1) {
-					String output=""+current+next; 
-					size++; 
-					dictionary.put(output,size); 
-					tracker.add(current); 
+			String current=word.substring(0,1); // gets the first word of the String 
+			String next=word.substring(1,2); // gets the second word of the String
+			while (word.length()>0) { 
+				if (dictionary.containsKey(""+current+next)==false && word.length()>1) { // checks to see if current + next is in the dictionary 
+					String output=""+current+next; // adds current + next together 
+					size++; // increments the size of the dictionary 
+					dictionary.put(output,size); // adds the output into the dictionary 
+					tracker.add(current); // adds the current into a tracker to be eventually outputed 
 					word=word.substring(current.length()); 
 					current=word.substring(0,1); 
 					if (word.length()>1) {
