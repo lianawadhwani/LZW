24187d5d124bd9e3b00f78e9db66cdb13351217e
Aaron Cho <aars.cho10@gmail.com>
2021-09-22 22:16:04 -0700

    Made method, algorithm() use a StringBuilder rather than a String
    The first idea that came to mind when trying to optimize. Very minor change and I can't tell if there was much of a change. The timer is ± 1 ms having times range from around 2.5 ms to 3.5 from what I have seen from running it on a file many times. I will try other ideas to optimize.
    
    After more time spent with the code and understanding it, there are more tasks to be done. Namely:
    
    - editing the decompression method to read in compressed file outputs and use data from the files rather than stored private instance variables.

--- src/LZW.java
+++ src/LZW.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class LZW {
 	
-	private String word; 
+	private StringBuilder word; 
 	private int size=255; 
 	private HashMap <String,Integer> dictionary = new HashMap <String,Integer> (); 
 	private ArrayList <String> tracker = new ArrayList <String> (); // this keeps track of the output 
@@ -21,11 +21,12 @@ public class LZW {
 	 * @throws IOException
 	 */
 	public LZW (String filename) throws IOException{
-		word=""; // initializes word
+		word = new StringBuilder();
+		word.append(""); // initializes word
 		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filename))); // this line reads the file
 		String line = reader.readLine(); 
 		while (line != null) { // reads the file 
-			word+=line; 
+			word.append(line); 
 			line= reader.readLine();
 		}
 		for (int i=0; i<256; i++) { // creates the dictionary and puts the values in it 
@@ -38,18 +39,22 @@ public class LZW {
 	 * Main method that encodes a message.
 	 */
 	public void algorithim () {
+		
 		String current=word.substring(0,1); // gets the first word of the String 
 		String next=word.substring(1,2); // gets the second word of the String
+		
 		while (word.length()>0) { 
 			if (dictionary.containsKey(""+current+next)==false && word.length()>1) { // checks to see if current + next is in the dictionary 
 				String output=""+current+next; // adds current + next together 
 				size++; // increments the size of the dictionary 
 				dictionary.put(output,size); // adds the output into the dictionary 
-				tracker.add(current); // adds the current into a tracker to be eventually outputed 
-				word=word.substring(current.length()); 
+				tracker.add(current); // adds the current into a tracker to be eventually output 
+				StringBuilder temp = new StringBuilder();
+				temp.append(word.substring(current.length()));
+				word=temp; 
 				current=word.substring(0,1); 
 				if (word.length()>1) {
-					next=word.substring(1,2); // create an if statment to deal with
+					next=word.substring(1,2); // create an if statement to deal with
 				}
 			} else if (word.length()>1 && dictionary.containsKey(""+current+next)){
 				current=""+current+next; 
@@ -58,7 +63,9 @@ public class LZW {
 			}
 			else {
 				tracker.add(current);
-				word=""; 
+				StringBuilder temp = new StringBuilder();
+				temp.append("");
+				word=temp; 
 			}
 		}
 
@@ -100,7 +107,6 @@ public class LZW {
 
 			}
 
-
 		}
 
 
@@ -124,22 +130,33 @@ public class LZW {
 		}
 		return (num); 
 	}
+	
+//	public void outputEncoded () {
+//		
+//	}
+//	
+//	public void outputDecoded() {
+//		
+//	}
 
 	public static void main (String [] args) throws IOException {
 		
-		long startTime = System.nanoTime();
+		long startTimeNano = System.nanoTime();
+		long startTimeMillis = System.currentTimeMillis();
 
 		LZW word = new LZW ("lzw-file1.txt"); 
 		word.algorithim(); 
 		
-		System.out.println(word.numbers()); 
-		System.out.println(word.nums); //changed nums to public to check this statement, then chaged it back to private
+		System.out.println(word.numbers());
+		System.out.println(word.nums); //changed nums to public to check this statement, then changed it back to private
 		System.out.println(word.decompress1()); 
 		System.out.println("Hello World");
 
-		long endTime = System.nanoTime();
+		long endTimeNano = System.nanoTime();
+		long endTimeMillis = System.currentTimeMillis();
 
-		System.out.print("Total time: " + (endTime-startTime) + " nanoseconds.");
+		System.out.println("Total time: " + (endTimeNano-startTimeNano) + " nanoseconds.");
+		System.out.println("Total time: " + (endTimeMillis-startTimeMillis) + " milliseconds.");
 	}
 }
 
